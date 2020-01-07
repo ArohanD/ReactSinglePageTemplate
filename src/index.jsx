d34b3bf28821a2e4580e039fd34f522d86ea8e4b
Arohan Dutt <arohandutt@gmail.com>
2020-01-06 20:01:42 -0600

    Modify template

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from'./router.jsx';
-//if you have a theme library, import it here
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div>hi</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
